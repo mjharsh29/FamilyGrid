0f3c61d9d514a0cd1cc8bec709476e0689d466e2
mjharsh29 <64350287+mjharsh29@users.noreply.github.com>
2020-04-26 13:23:14 +0530

    Adding Initial files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <FamilyGrid />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+import FamilyGrid from './components/FamilyGrid.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FamilyGrid
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
